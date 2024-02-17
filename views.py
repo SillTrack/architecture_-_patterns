10663fbc64505b367c1102822070699bd5831090
HandsomeStranger <manuxin2002@bk.ru>
2024-02-17 16:33:43 +0300

    [initial commit] Task 1

---
+++ views.py
@@ -0,0 +1,20 @@
+from typing import Any
+from Hands_framework.templator import render
+
+
+class Index:
+    def __call__(self, request):
+        return '200 OK', render('index.html', date=request.get('date', None))
+
+
+class About:
+    def __call__(self, request):
+        return '200 OK', 'about'
+    
+class Contacts:
+    def __call__(self, request):
+        return '200 OK', render('contacts.html', data=request.get('date', None), key=request.get('key', None))
+    
+class Basket:
+    def __call__(self, request):
+        return '200 OK', render('basket.html', data=request.get('date', None), key=request.get('key', None))
